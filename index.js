744bf247b0bb879f59a6aa1e9650651bdf686b24
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-03-17 08:40:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+// Define the variables
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
+// Import dependencies
+const fs = require('fs');
+const path = require('path');
+
+// Read the file contents
+const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8');
+
+// Run the tests
+describe('index.js', function () {
+  describe('companyName', function () {
+    it('is set as Scuber', function () {
+      expect(companyName).to.equal('Scuber');
+    });
+
+    it('is defined as a const', function () {
+      expect(js).to.match(/const companyName/, "Expected companyName to be a const");
+    });
+  });
+
+  describe('mostProfitableNeighborhood', function () {
+    it('is declared as equal to Chelsea', function () {
+      expect(mostProfitableNeighborhood).to.equal('Chelsea');
+    });
+
+    it('is defined using let', function () {
+      expect(js).to.match(/let mostProfitableNeighborhood/, "Expected mostProfitableNeighborhood to be defined using let");
+    });
+  });
+
+  describe('companyCeo', function () {
+    it('is declared as equal to Susan Smith', function () {
+      expect(companyCeo).to.equal('Susan Smith');
+    });
+
+    it('is defined using let', function () {
+      expect(js).to.match(/let companyCeo/, "Expected companyCeo to be defined using let");
+    });
+  });
+});
+
